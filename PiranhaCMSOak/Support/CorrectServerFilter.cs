abf201f5aac75ee89e6ccd23f05cdab225e19c46
davidpodhola <david.podhola@naseukoly.cz>
2013-08-11 21:36:22 +0200

    CorrectServerFilter has a constructor with parameters

--- PiranhaCMSOak/Support/CorrectServerFilter.cs
+++ PiranhaCMSOak/Support/CorrectServerFilter.cs
@@ -9,6 +9,15 @@ namespace PiranhaCMSOak.Support
     // http://stackoverflow.com/questions/14540899/mvc4-custom-onactionexecuting-global-asx-filter-is-not-being-triggered
     public class CorrectServerFilter : IActionFilter
     {
+        string _hostChecked;
+        string _urlRequired;
+
+        public CorrectServerFilter(string hostChecked, string urlRequired)
+        {
+            this._hostChecked = hostChecked;
+            this._urlRequired = urlRequired;
+        }
+
         public void OnActionExecuted(ActionExecutedContext filterContext)
         {
             return;
@@ -30,9 +39,9 @@ namespace PiranhaCMSOak.Support
             if (ctx != null)
             {
                 string host = ctx.Request.Url.Host;
-                if (host != "structureddocument.com")
+                if (host.CompareTo(_hostChecked) != 0 )
                 {
-                    ctx.Response.Redirect("https://structureddocument.com");
+                    ctx.Response.Redirect(_urlRequired);
                     ctx.Response.End();
                 }
             }
