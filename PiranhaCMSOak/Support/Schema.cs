a1c4811d81bb2543247025fd1ab16f50a6e828b1
davidpodhola <david.podhola@naseukoly.cz>
2013-08-12 14:07:24 +0200

    Schema for users added

---
+++ PiranhaCMSOak/Support/Schema.cs
@@ -0,0 +1,23 @@
+﻿using Oak;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiranhaCMSOak.Support
+{
+    public class Schema
+    {
+        public string CreateUserTable(Seed seed)
+        {
+            return seed.CreateTable("Users",
+                new { Id = "int", Identity = true, PrimaryKey = true },
+                new { Email = "nvarchar(max)" },
+                new { FirstName = "nvarchar(max)" },
+                new { LastName = "nvarchar(max)" },
+                new { Password = "nvarchar(max)" }
+            );
+        }
+    }
+}
