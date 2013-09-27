adcbf6b2ae7862c83cf160821e25ebbad6b2e9c2
David Podhola <david.podhola@naseukoly.cz>
2013-09-27 14:34:55 +0200

    tests corrected

--- PiranhaCMSOak.Tests/describe_Reflection.cs
+++ PiranhaCMSOak.Tests/describe_Reflection.cs
@@ -14,19 +14,30 @@ namespace PiranhaCMSOak.Tests
         public class A
         {
             public string B { get; set; }
+            public string D { get; set; }
+            public string E { get; set; }
         }
         public class C
         {
             public string B { get; set; }
+            public string D { get; set; }
+            public string E { get; set; }
         }
 
         void specify_test_CopyProperties()
         {
             A a = new A();
             a.B = "b";
+            a.D = "d";
+            a.E = "e";
             C c = new C();
+            c.B = "b1";
+            c.D = "d1";
+            c.E = "e1";
             a.CopyProperties(c);
-            a.B.should_be("b");
+            c.B.should_be("b");
+            c.D.should_be("d");
+            c.E.should_be("e");
         }
     }
 }
